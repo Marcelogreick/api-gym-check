306e4b1578b70d4a941fda7379ba27573ab7e05e
Marcelogreick <mgreick25@gmail.com>
2023-04-10 23:56:34 -0300

    validando hora do check in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -40,22 +40,22 @@ describe("Validate Check-in Use Case", () => {
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 
-  // it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
-  //   vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
 
-  //   const createdCheckIn = await checkInsRepository.create({
-  //     gym_id: "gym-01",
-  //     user_id: "user-01",
-  //   });
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
 
-  //   const twentyOneMinutesInMs = 1000 * 60 * 21;
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
 
-  //   vi.advanceTimersByTime(twentyOneMinutesInMs);
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
 
-  //   await expect(() =>
-  //     sut.execute({
-  //       checkInId: createdCheckIn.id,
-  //     })
-  //   ).rejects.toBeInstanceOf(Error);
-  // });
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
 });
