441f05b9df59a383fdc899437bbc30dc44ecce2a
Marcelogreick <mgreick25@gmail.com>
2023-04-25 23:38:27 -0300

    teste controller atutenticacao e2e

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -6,7 +6,7 @@ import { verifyJwt } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
-  app.post("/session", authenticate);
+  app.post("/sessions", authenticate);
 
   // Authenticate
 
