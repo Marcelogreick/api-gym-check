a1d2a9d70d6db3f8fc0d4936b24dac8370612d4f
Marcelogreick <mgreick25@gmail.com>
2023-04-28 11:20:13 -0300

    controller de historico de academia

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,9 @@
+import { verifyJwt } from "@/http/middlewares/verify-jwt";
+import { FastifyInstance } from "fastify";
+import { create } from "./create";
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook("onRequest", verifyJwt);
+
+  app.get("/gyms/:gymId/check-ins", create);
+}
