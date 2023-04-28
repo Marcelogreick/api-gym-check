b16b016d8dec3c268b4c6790a65b095add181219
Marcelogreick <mgreick25@gmail.com>
2023-04-28 10:48:15 -0300

    controller nearby

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,11 @@
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
+import { search } from "./search";
+import { nearby } from "./nearby";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJwt);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
 }
