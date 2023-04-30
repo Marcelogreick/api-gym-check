6653befb5a09a8ad1d01eb4113a8f974b54e2d31
Marcelogreick <mgreick25@gmail.com>
2023-04-30 17:49:11 -0300

    teste controller de busca  academia

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -2,7 +2,7 @@ import { verifyJwt } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
 
-export async function gymsRoutes(app: FastifyInstance) {
+export async function checkInRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJwt);
 
   app.get("/gyms/:gymId/check-ins", create);
