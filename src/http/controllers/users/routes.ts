bd2dbdf4f36f996d5840b53f8eaae9c7012d17b2
Marcelogreick <mgreick25@gmail.com>
2023-05-01 18:31:40 -0300

    refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,12 +3,14 @@ import { FastifyInstance } from "fastify";
 import { profile } from "./profile";
 import { authenticate } from "./authenticate";
 import { register } from "./register";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
 
-  // Authenticate
+  app.patch("/token/refresh", refresh);
 
+  // Authenticate
   app.get("/me", { onRequest: verifyJwt }, profile);
 }
